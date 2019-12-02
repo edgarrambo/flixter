5d3c904d4e294b2820aeeed88e9eb5805b32ab7e
edgarrambo <ragde956@gmail.com>
2019-12-02 21:41:49 +0000

    added links to student page

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,13 +5,13 @@ class Instructor::CoursesController < ApplicationController
 		@course = Course.new
 	end
 
-	def create
-		@course = current_user.courses.create(course_params)
-		if @course.valid?
-		redirect_to instructor_course_path(@course)
-	else
-		render :new, status: :unprocessable_entity
-	end
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
 	end
 
 	def show
